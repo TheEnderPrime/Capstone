0f48ac439a79f810e718037a695ac9fc88e42fcf
kokeshs <kokeshs@oregonstate.edu>
2018-02-01 09:31:27 -0800

    add kit base colors

---
+++ src/KiteTest/src/Colors/Colors.js
@@ -0,0 +1,26 @@
+const Colors = {
+	kite_green: '#30B75E',
+    kite_greenLight: '#84DFA3',
+    kite_greenMediumLight: '#52C97A',
+    kite_greenMediumDark: '#0BAA41',
+    kite_greenDark: '#047F2E',
+
+    kite_blue: '#30769F',
+    kite_blueLight: '#82B6D4',
+    kite_blueMediumLight: '#5090B6',
+    kite_blueMediumDark: '#126494',
+    kite_blueDark: '#09496F',
+
+    kite_pink: '#FA5E41',
+    kite_pinkLight: '#FFA797',
+    kite_pinkMediumLight: '#FF7F68',
+    kite_pinkMediumDark: '#E83210',
+    kite_pinkDark: '#AD1F05',
+
+    kite_orange: '#FAA741',
+    kite_orangeLight: '#FFD097',
+    kite_orangeMediumLight: '#FFBB68',
+    kite_orangeMediumDark: '#E88710',
+    kite_orangeDark: '#AD6205',
+};
+module.exports = Colors;
--- src/KiteTest/src/components/Welcome/styles.js
+++ src/KiteTest/src/components/Welcome/styles.js
@@ -1,12 +1,12 @@
 import {StyleSheet} from 'react-native';
-
+import Colors from '../../Colors/Colors';
 module.exports = styles = StyleSheet.create({	
 	container: {
 		flex: 1,
 		flexDirection: 'column', //column or row
 		justifyContent: 'space-around', //main axis (the flexDirection) =>center, space-around, space-between
     	alignContent: 'center', // cross axis (opposite of the flexDirection) => 
-    	backgroundColor: '#A6CFBD',
+    	backgroundColor: Colors.kite_greenLight,
   	},
   	logo: {
 		// this will contain the logo and welcome words
@@ -19,7 +19,7 @@ module.exports = styles = StyleSheet.create({
 		marginBottom: 15,
     	fontSize: 40,
     	textAlign: 'center',
-		color: '#277552'
+		color: Colors.kite_greenDark
   	}, 
   	welcome: {
 		flex: 1,
@@ -30,7 +30,7 @@ module.exports = styles = StyleSheet.create({
 		marginTop: 10,
 		
 		fontSize: 30,
-		color: '#277552',
+		color: Colors.kite_greenDark,
 	},
 	selectionBox: { // this will contain both of the signin and signup selections containers 
 		flex: 1,
@@ -43,7 +43,7 @@ module.exports = styles = StyleSheet.create({
 	selections: {
 		// this wil contain each of the signin and signup texts and might change the text to be dark if needed
 		fontSize: 20,
-		color: '#277552'
+		color: Colors.kite_greenm
 	},
 	loginBox: {
 		flex: 2,
