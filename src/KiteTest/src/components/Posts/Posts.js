773feb171646bf3da843bef648272a088d015c33
TheEnderPrime <dbaugh.oregon@gmail.com>
2018-03-16 19:19:44 -0700

    ANDREW PUT YOUR STUFF

--- src/KiteTest/src/components/Posts/Posts.js
+++ src/KiteTest/src/components/Posts/Posts.js
@@ -17,16 +17,6 @@ class Posts extends React.Component {
 
 	constructor(){
 		super()
-		this.onEventPress = this.onEventPress.bind(this)
-		this.data = [
-			{time: '09:00', title: 'Newest Post ', description: 'Event 1 Description'},
-		    {time: '10:45', title: 'Post 2', description: 'Event 2 Description'},
-		    {time: '12:00', title: 'Post 3', description: 'Event 3 Description'},
-		    {time: '14:00', title: 'Post 4', description: 'Event 4 Description'},
-			{time: '16:30', title: 'Post 5', description: 'Event 5 Description'},
-			{time: '14:00', title: 'Oldest Post 6', description: 'Event 6 Description'},
-		]
-		this.state = {selected: null}
 	}
 
 	onRefresh(){
@@ -47,35 +37,19 @@ class Posts extends React.Component {
 	}
 
 	onEventPress(data){
-    this.setState({selected: data})
-	//this.props.navigation.navigate("Posts")
+    //this.props.navigation.navigate("Posts")
   }
 
  	render() {
 		return (
 			<View style={styles.container}>
 				<View style={styles.header}>
-					<Text>Event Title</Text>
+					<Text>Post Title Here</Text>
 					<Text>Overall Likes and Comments</Text>
 				</View>
 
 				<View style={styles.postTimeline}>
-					<Timeline
-						style={styles.timelineList}
-						data={this.data}
-						circleSize={20}
-						circleColor='rgb(45,156,219)'
-						lineColor='rgb(45,156,219)'
-						timeContainerStyle={{minWidth:52, marginTop: -5}}
-						timeStyle={{textAlign: 'center', backgroundColor:'#ff9797', color:'white', padding:5, borderRadius:13}}
-						descriptionStyle={{color:'gray'}}
-						timeContainerStyle={{minWidth:72}}
-						circleSize={-100}
-						showTime={false}
-						onEventPress={this.onEventPress}
-						
-						
-					/>
+					<Text>PUT YOUR SHIZ HERE ANDREW</Text>
 				</View>
 			</View>
 
